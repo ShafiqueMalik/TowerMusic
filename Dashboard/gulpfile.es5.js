208b34e390bc1ab53738911d835e17dc163094e3
Shafiq <shafiq@gmail.com>
2020-10-20 23:03:11 +0500

    Add project files.

---
+++ Dashboard/gulpfile.es5.js
@@ -0,0 +1,13 @@
+﻿/// <binding AfterBuild='build-sass, default' />
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+gulp.task('build-sass', function () {
+    return gulp.src('./sass/**/*.scss').pipe(sass()).pipe(gulp.dest('./css'));
+});
+
+//default task
+gulp.task('default', ['build-sass']);
+
---
+++ Dashboard/js/app.js
@@ -0,0 +1,24 @@
+﻿function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    
+    reader.onload = function(e) {
+        $('.avatar-preview').css('background-image', "url(" + e.target.result + ")");
+        $(".avatar-preview .fa").hide();
+    }
+    
+    reader.readAsDataURL(input.files[0]); // convert to base64 string
+  }
+}
+
+
+
+$(document).ready(function () {
+    $("#avatar").change(function () {
+        readURL(this);
+    });
+    $(".avatar-preview").click(function () {
+        $(".plus-con").trigger("click");
+        $(this).off("click");
+    })
+})
